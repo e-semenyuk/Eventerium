80613feb46ca0ebba97349d1f7199463ee597f8a
Eugene <eugene.semenyuk@student.ehu.lt>
2023-12-07 16:40:21 +0300

    Add delete button for events

--- src/screens/EventDetailsScreen.js
+++ src/screens/EventDetailsScreen.js
@@ -1,9 +1,61 @@
 // src/screens/EventDetailsScreen.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, Alert } from 'react-native';
+import SQLite from 'react-native-sqlite-storage';
 
-const EventDetailsScreen = ({ route }) => {
+const EventDetailsScreen = ({ route, navigation }) => {
   const { event } = route.params;
+
+  // Function to handle the delete event action
+  const handleDeleteEvent = () => {
+    // Show a confirmation prompt before deleting the event
+    Alert.alert(
+      'Confirm Deletion',
+      `Are you sure you want to delete the event "${event.title}"?`,
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Delete',
+          onPress: () => performDeleteEvent(),
+          style: 'destructive',
+        },
+      ]
+    );
+  };
+
+  // Function to perform the actual deletion of the event from the database
+  const performDeleteEvent = () => {
+    const db = SQLite.openDatabase({ name: 'events.db', createFromLocation: 1 });
+
+    // SQL statement to delete the event from the database
+    const deleteEventStatement = 'DELETE FROM events WHERE id = ?';
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        deleteEventStatement,
+        [event.id],
+        (tx, results) => {
+          // Check if the event was successfully deleted
+          if (results.rowsAffected > 0) {
+            Alert.alert('Event Deleted', 'The event has been successfully deleted.', [
+              {
+                text: 'OK',
+                onPress: () => navigation.navigate('EventView'),
+              },
+            ]);
+          } else {
+            Alert.alert('Error', 'Failed to delete the event. Please try again.');
+          }
+        },
+        (error) => {
+          console.error('Error executing SQL statement:', error);
+        }
+      );
+    });
+  };
 
   return (
     <View>
@@ -11,10 +63,17 @@ const EventDetailsScreen = ({ route }) => {
         Event Details
       </Text>
       <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{`Title: ${event.title}`}</Text>
-        <Text>{`Date: ${event.date}`}</Text>
-        <Text>{`Time: ${event.time}`}</Text>
-        <Text>{`Type: ${event.type}`}</Text>
-        <Text>{`Description: ${event.description}`}</Text>
+      <Text>{`Date: ${event.date}`}</Text>
+      <Text>{`Time: ${event.time}`}</Text>
+      <Text>{`Type: ${event.type}`}</Text>
+      <Text>{`Description: ${event.description}`}</Text>
+
+      {/* Add the "Delete Event" button */}
+      <Button
+        title="Delete Event"
+        onPress={handleDeleteEvent}
+        color="red"
+      />
     </View>
   );
 };
