067e33359e07f61719d74fb5a94b63d52088c440
Eugene <eugene.semenyuk@student.ehu.lt>
2024-01-13 14:44:14 +0300

    Add tags for new templates

--- src/screens/templates/NewTemplateScreen.js
+++ src/screens/templates/NewTemplateScreen.js
@@ -1,31 +1,30 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, TextInput, Button, Alert, Switch, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, Text, TextInput, Button, Alert, Switch, ScrollView, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { showMessage } from 'react-native-flash-message';
 import { useTranslation } from 'react-i18next';
 
-const NewTemplateScreen = ({ route, onRequestClose, selectedTasks }) => {
+const NewTemplateScreen = ({ onRequestClose, selectedTasks }) => {
   const tasks = selectedTasks;
   const [name, setName] = useState('');
-  const [type, setType] = useState('');
+  const [type, setType] = useState([]);
+  const [tagInput, setTagInput] = useState('');
   const [description, setDescription] = useState('');
   const nameInputRef = useRef(null);
   const { t } = useTranslation();
-  const [isPublic, setIsPublic] = useState(false); // New state for the toggle switch
+  const [isPublic, setIsPublic] = useState(false);
 
   useEffect(() => {
-    // Set focus to the TextInput when the component mounts
     nameInputRef.current.focus();
   }, []);
 
   const handleAddTemplate = async () => {
-    if (!name || !type) {
+    if (!name || !type.length) {
       Alert.alert('Error', t('Please fill in all fields.'));
       return;
     }
 
     try {
-      // Make a POST request to create a new template
       const templateResponse = await fetch('https://crashtest.by/app/templates.php', {
         method: 'POST',
         headers: {
@@ -33,9 +32,10 @@ const NewTemplateScreen = ({ route, onRequestClose, selectedTasks }) => {
         },
         body: JSON.stringify({
           name,
-          type,
+          type: type.join(' '), // Join the array into a string
           description,
-          isPublic: isPublic,
+          isPublic,
+          tags: type
         }),
       });
 
@@ -43,7 +43,6 @@ const NewTemplateScreen = ({ route, onRequestClose, selectedTasks }) => {
         const templateData = await templateResponse.json();
         const templateId = templateData.id;
 
-        // Make a batch of POST requests to create tasks associated with the template
         await Promise.all(
           tasks.map(async (task, index) => {
             const taskResponse = await fetch('https://crashtest.by/app/templateDetails.php', {
@@ -57,7 +56,7 @@ const NewTemplateScreen = ({ route, onRequestClose, selectedTasks }) => {
                 priority: task.priority,
                 type: task.type,
                 orderId: task.orderId,
-                templateId: templateId,
+                templateId,
                 color: task.color,
               }),
             });
@@ -91,15 +90,20 @@ const NewTemplateScreen = ({ route, onRequestClose, selectedTasks }) => {
     onRequestClose();
   };
 
+  const handleTagInputChange = (text) => {
+    setTagInput(text);
+  };
+
+  const handleAddTag = () => {
+    if (tagInput.trim() !== '') {
+      setType([...type, tagInput.trim()]);
+      setTagInput('');
+    }
+  };
+
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{ flex: 1 }}
-    >
-      <ScrollView
-        keyboardShouldPersistTaps="always"
-        contentContainerStyle={{ flex: 1, justifyContent: 'flex-end', padding: 16, paddingBottom: 0 }}
-      >
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
+      <ScrollView keyboardShouldPersistTaps="always" contentContainerStyle={{ flex: 1, justifyContent: 'flex-end', padding: 16, paddingBottom: 0 }}>
         <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', borderTopLeftRadius: 20, borderTopRightRadius: 20, padding: 16 }}>
           <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
             <Text style={{ fontSize: 24, fontWeight: 'bold', flex: 1, textAlign: 'center' }}>
@@ -107,30 +111,44 @@ const NewTemplateScreen = ({ route, onRequestClose, selectedTasks }) => {
             </Text>
             <Icon name="close" size={24} onPress={onRequestClose} style={{ marginLeft: 'auto' }} />
           </View>
-          <TextInput
-            ref={nameInputRef}
-            placeholder={t('Template Name')}
-            value={name}
-            onChangeText={setName}
-            style={{ paddingTop: 16, padding: 8, fontWeight: 'bold', fontSize: 20 }}
-          />
-          <TextInput
-            placeholder={t('Template Type')}
-            value={type}
-            onChangeText={setType}
-            style={{ paddingTop: 16, padding: 8 }}
-          />
-          <TextInput
-            placeholder={t('Template Description')}
-            value={description}
-            onChangeText={setDescription}
-            style={{ paddingTop: 16, padding: 8 }}
-          />
+          <TextInput ref={nameInputRef} placeholder={t('Template Name')} value={name} onChangeText={setName} style={{ paddingTop: 16, padding: 8, fontWeight: 'bold', fontSize: 20 }} />
+          <TextInput placeholder={t('Template Description')} value={description} onChangeText={setDescription} style={{ paddingTop: 16, padding: 8 }} />
 
           <View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 16 }}>
             <Text style={{ marginRight: 8 }}>{t('Public')}</Text>
             <Switch value={isPublic} onValueChange={(value) => setIsPublic(value)} />
           </View>
+
+          <View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 16 }}>
+            <TextInput
+              placeholder={t('Add Tag')}
+              value={tagInput}
+              onChangeText={handleTagInputChange}
+              style={{ flex: 1, padding: 8, marginRight: 8, borderWidth: 1, borderColor: 'gray', borderRadius: 5 }}
+            />
+            <TouchableOpacity onPress={handleAddTag} style={{ padding: 8, backgroundColor: '#007BFF', borderRadius: 5 }}>
+              <Text style={{ color: 'white' }}>{t('Add')}</Text>
+            </TouchableOpacity>
+          </View>
+
+          <ScrollView horizontal style={{ flexDirection: 'row', paddingTop: 16 }}>
+            {type.map((tag, index) => (
+              <TouchableOpacity
+                key={index}
+                style={{ borderRadius: 15, backgroundColor: 'lightgray', padding: 8, marginRight: 8 }}
+                onPress={() => {
+                  const updatedTags = [...type];
+                  updatedTags.splice(index, 1);
+                  setType(updatedTags);
+                }}
+              >
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Text>{tag}</Text>
+                <Icon name="times" size={12} color="red" />
+                </View>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
 
           <Button title={t('Add')} onPress={handleAddTemplate} />
           <Button title={t('Cancel')} onPress={handleCancel} color="gray" />
